87006e4c0fa893724adb11c3703d4ad5ee98e156
talitaspadaccini <tspadaccini1990@gmail.com>
2022-12-07 15:59:11 -0300

    page-videos

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,19 +21,75 @@ function closeForm(){
 }
 
 /* Troca e abertura de páginas */
-
-anterior = 'page-home'
+anterior = 'page-home';
 
 function select(objeto) {
       var el1 = document.getElementById(objeto);
       var el2 = document.getElementById(anterior);
       if (anterior != objeto ) {
             el1.style.display = 'block';
-            el2.style.display = 'none'
-      }
-      else if (anterior == objeto){
-            el1.style.display = 'none';
-            el2.style.display = 'block';
+            el2.style.display = 'none';
       }
       anterior = objeto;
 }
+
+
+/* INDEX - PAGE-HOME */
+function abrirVideoDestaque(){
+    window.open(
+        'https://youtu.be/E8LqVj4vMuY',
+        'Vídeo Destaque'
+    )
+    return false;
+}
+
+/* INDEX - PAGE-VIDEOS */
+
+/* Carregando vídeo destaque */
+
+// Esse código carrega o código da API do IFrame Player de forma assíncrona.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Esta função cria um <iframe> (e player do YouTube) 
+// após o download do código da API.
+var player;
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+        videoId: 'rlZO8uoAmjw',
+        playerVars: {
+            'controls': 0,
+            'rel': 0,
+            'showinfo': 1,
+            'mute': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+// A API chamará essa função quando o player de vídeo estiver pronto.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// A API chama essa função quando o estado do player muda. 
+// A função indica que ao reproduzir um vídeo (estado=1), 
+// o player deve reproduzir por 35 segundos e depois parar.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 35000);
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
